4c1d77d74ede14d3d58742baa0c963c7c9f68e10
cataldo-lab <cataldobenjamin@gmail.com>
2025-04-25 22:19:25 -0400

    primer commit

---
+++ src/main.tsx
@@ -0,0 +1,52 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from '@pages/Login';
+import Home from '@pages/Home';
+import Users from '@pages/Users';
+import Register from '@pages/Register';
+import Error404 from '@pages/Error404';
+import Root from '@pages/Root';
+import ProtectedRoute from '@components/ProtectedRoute'; // Assuming components is relative to src
+import '@styles/styles.css';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <Error404 />,
+      children: [
+        {
+          path: '/home',
+          element: <Home />,
+        },
+        {
+          path: '/users',
+          element: (
+            <ProtectedRoute allowedRoles={['administrador']}>
+              <Users />
+            </ProtectedRoute>
+          ),
+        },
+      ],
+    },
+    {
+      path: '/auth',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true, // Habilita startTransition
+      v7_relativeSplatPath: true, // Habilita resolución relativa en splats
+    },
+  }
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
