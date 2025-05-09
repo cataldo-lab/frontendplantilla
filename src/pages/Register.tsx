b85c6790db5a17918034d55ec546adf7d412643e
cataldo-lab <cataldobenjamin@gmail.com>
2025-05-09 17:36:54 -0400

    Se agrego -jsx para disminuir los errores en fronted

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { register } from '@services/auth.service.js';
 import Form from "@components/Form";
 import useRegister from '@hooks/auth/useRegister.jsx';
-import { showErrorAlert, showSuccessAlert } from '@helpers/sweetAlert.js';
+import { showErrorAlert, showSuccessAlert } from '@helpers/sweetAlert';
 import '@styles/form.css';
 
 const Register = () => {
